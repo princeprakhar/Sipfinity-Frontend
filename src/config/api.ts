6fd5b3415b204e27190bc2fb65b623e898ae32c4
princeprakhar <prakhardeep173@gmail.com>
2025-06-12 14:04:41 +0530

    wired with the backend endpoint

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 // src/config/api.ts
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://ecommerce-backend-production-8ac4.up.railway.app/api/v1"||'http://localhost:8080/api/v1';
 
 export const API_ENDPOINTS = {
   AUTH: {
