65e4502f7b5e7ed1180933c2670ff77e59df7b2c
princeprakhar <prakhardeep173@gmail.com>
2025-06-18 04:33:32 +0530

    added vercel analytic

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,10 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 
+import { inject } from '@vercel/analytics'
+inject()
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
